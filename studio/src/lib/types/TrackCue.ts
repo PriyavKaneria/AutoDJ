dda8cd2498f63f79924fad4391a8eefda619a866
Priyav Kaneria <priyavkaneria@gmail.com>
2024-06-11 17:38:45 +0530

    Better type desciptions to prevent confusion

--- studio/src/lib/types/TrackCue.ts
+++ studio/src/lib/types/TrackCue.ts
@@ -1,9 +1,9 @@
 export interface TrackCue {
-	url: string;
-	startFrom: number;
-	cueFrom: number;
-	cueTo?: number;
-	duration?: number;
-	audioElement?: HTMLAudioElement;
-	scrollX?: number;
+	url: string; // URL of the audio file
+	startFrom: number; // Where the track position should start from
+	cueFrom: number; // Where the track should start playing from
+	cueTo?: number; // Where the track should stop playing
+	duration?: number; // Duration of the track
+	audioElement?: HTMLAudioElement; // Reference to the audio element
+	scrollX?: number; // Scroll position on the X-axis
 }
